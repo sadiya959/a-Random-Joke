4ad2e965532ee13241117b79f8573982e4ff1ace
sadiya959 <sacdamamoud@gmail.com>
2025-04-25 21:48:49 +0300

    added  spinner and display  history jokes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const preJokes = [];
+
 async function getJoke() {
     const jokePara = document.getElementById("joke");
-    jokePara.textContent = "Loading... 🤔";
+    const jokeHistory = document.getElementById("joke-history");
+    const spinner = document.getElementById("spinner");
+    jokePara.textContent = "";
+
+
+
+    spinner.classList.remove("hidden")
+
   
     try {
       const response = await fetch("https://icanhazdadjoke.com/", {
@@ -10,7 +19,28 @@ async function getJoke() {
       });
   
       const data = await response.json();
-      jokePara.textContent = data.joke;
+      const newJoke = data.joke
+      
+      spinner.classList.add("hidden")
+      jokePara.textContent = newJoke.joke;
+      
+      preJokes.unshift(newJoke);
+      if (preJokes.length > 5) preJokes.pop();
+
+
+
+
+      jokeHistory.innerHTML = ""
+
+      preJokes.forEach(joke => {
+        jokeList = document.createElement("li")
+        jokeList.textContent = joke
+
+        jokeHistory.appendChild(jokeList)
+
+      })
+
+
     } catch (error) {
       jokePara.textContent = "Oops! Couldn't fetch a joke 😓";
       console.error("Error:", error);
